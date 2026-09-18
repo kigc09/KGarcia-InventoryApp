05510383004113cabb5ad28df6e79300e0f352f0
kigc09 <kgarciacastrejon@mail.mccneb.edu>
2026-09-18 17:07:03 -0500

    First Commit - Finished Part 1 of the assignment

---
+++ src/main/java/com/example/kgarciainventoryapp/KGarciaInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.kgarciainventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KGarciaInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KGarciaInventoryAppApplication.class, args);
+	}
+
+}
